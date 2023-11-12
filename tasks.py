b59008222bb38f8b584948d4bbc3afc2faeb1b8f
Milad <khaghanim2@hotmail.com>
2023-11-12 02:48:04 +0100

    implement read data from firebase

--- tasks.py
+++ tasks.py
@@ -27,12 +27,16 @@ def get_deviceID():
 
     return device_id
 
+device_id = get_deviceID()
 def init_firebase():
     # Replace with your Firebase project credentials file
     cred = credentials.Certificate("./cred/cred.json")
     firebase_admin.initialize_app(cred,{"databaseURL": "https://pythonrobocorp-default-rtdb.europe-west1.firebasedatabase.app/"})
     return db.reference('/')
 
+def read_machine_data():
+    ref = db.reference('/'+device_id)
+    return ref.get()
 
 # retrieving data from root node of firebase
 def firebase_readDate():
@@ -197,31 +201,37 @@ def load_page():
         logging.info("could not load the page")
 
 # select fields
-def select_field():
+def select_field(country,person,isNotAlone,request,type,service,isAlone):
     logging.info("\nStart select field")
     browser.set_selenium_implicit_wait(30)
     load_page()
     browser.wait_until_element_is_visible('id:xi-sel-400',30)
     logging.info("try to select country")
-    browser.select_from_list_by_label("id:xi-sel-400","Argentinien")
+    browser.select_from_list_by_label("id:xi-sel-400",country)
     browser.wait_until_element_is_visible('id:xi-sel-422',30)
     logging.info("try to select person")
-    browser.select_from_list_by_label("id:xi-sel-422","eine Person")
+    browser.select_from_list_by_label("id:xi-sel-422",person)
     logging.info("select ja or nein")
-    browser.select_from_list_by_label("id:xi-sel-427","nein")
+    browser.select_from_list_by_label("id:xi-sel-427",isNotAlone)
     logging.info("select request")
-    browser.click_element('//*[@id="xi-div-30"]/div[2]/label')
+    print(read_machine_data()["request"])
+    browser.click_element(request)
+    #browser.click_element('//*[@id="xi-div-30"]/div[2]/label')
     logging.info("select type")
     try:
-        browser.click_element('//*[@id="inner-323-0-2"]/div/div[1]/label')
+        browser.click_element(type)
+        #browser.click_element('//*[@id="inner-323-0-2"]/div/div[1]/label')
     except:
         logging.info("select ja ")
-        browser.select_from_list_by_label("id:xi-sel-427","ja")
+        #browser.select_from_list_by_label("id:xi-sel-427","ja")
+        browser.select_from_list_by_label("id:xi-sel-427",isAlone)
         raise Exception("fail click type")
     #browser.click_element('//*[@id="inner-439-0-2"]/div/div[7]/label')
     logging.info("select service")
     try:
-        browser.click_element('//*[@id="SERVICEWAHL_DE323-0-2-3-328338"]')
+        browser.click_element('//*[@data-tag0="'+service+'"]')
+        #browser.click_element('//*[@id="SERVICEWAHL_DE323-0-2-3-328338"]')
+        #browser.click_element(read_machine_data()["service"])
     except:
         logging.info("click failed.try again")
 
@@ -269,7 +279,7 @@ def session_is_not_expired():
     return result
 
 # run the main function
-def main(expected_remaining_time):
+def main(expected_remaining_time,country,person,isNotAlone,request,type,service,isAlone):
     try:
         run_task = True
         click_next = True
@@ -293,7 +303,7 @@ def main(expected_remaining_time):
             while(retry_counter < 3):
                 try:
                     retry_counter += 1
-                    select_field()
+                    select_field(country,person,isNotAlone,request,type,service,isAlone)
                     run_task = True
                     click_next = True
                     break
@@ -308,6 +318,7 @@ def main(expected_remaining_time):
                     appointment_sound()
                     run_task = False
                     click_next = False
+                    firebase_sendData("Appointment available on machine  : " + get_deviceID() + " for person" )
                 else:
                     browser.set_selenium_implicit_wait(30)
                     # if session is expired, start from navigate again
@@ -331,12 +342,20 @@ def main(expected_remaining_time):
         else:
             logging.error("bot faced error: ")
 
+
+
+def is_machine_allowed():
+    allowd_machine_list = firebase_readDate()
+    return allowd_machine_list[get_deviceID()] == "allowed"
+
+
+    
 if __name__ == "__main__":
     init_firebase()
-    firebase_sendData(get_deviceID())
-    print(firebase_readDate())
-    device_id = get_deviceID()
-    if(device_id == "{293A6EE2-CB53-4420-8C5D-529C9EC990AC}" or device_id == "{956219e5-1e50-4492-8903-8e8e203ce095}"):
+    firebase_sendData("The bot started on Machine : " + get_deviceID())
+    #if(device_id == "{293A6EE2-CB53-4420-8C5D-529C9EC990AC}" or device_id == "{956219e5-1e50-4492-8903-8e8e203ce095}"):
+    if(is_machine_allowed()):
+        firebase_sendData("Machine is allowed")
         # Create a ConfigParser object
         config = configparser.ConfigParser()
         # Read the configuration file
@@ -356,6 +375,14 @@ if __name__ == "__main__":
         setup_logging()
         if(verbus=="False"):
             disable_logging()
-        main(expected_remaining_time)
+
+        country = read_machine_data()["country"]
+        person = read_machine_data()["person"]
+        isNotAlone = read_machine_data()["!isAlone"]
+        request = read_machine_data()["request"]
+        type = read_machine_data()["type"]
+        service = read_machine_data()["service"]
+        isAlone = read_machine_data()["isAlone"]
+        main(expected_remaining_time,country,person,isNotAlone,request,type,service,isAlone)
     else:
         print("this machine is not supported ")
